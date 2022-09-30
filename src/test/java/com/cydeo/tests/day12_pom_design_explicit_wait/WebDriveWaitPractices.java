4d83709458e5168e02c998567f72bad854185e51
ZalinaKabulova <105004544+ZalinaKabulova@users.noreply.github.com>
2022-09-30 08:56:49 +0200

    Initial commit

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/WebDriveWaitPractices.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/WebDriveWaitPractices.java
@@ -13,48 +13,40 @@ public class WebDriveWaitPractices {
 
     @Test
     public void dynamic_load_7_test(){
-
-        //        TC#4 : Dynamically Loaded Page Elements 7
-//        1. Go to https://practice.cydeo.com/dynamic_loading/7
+        //1. Go to https://practice.cydeo.com/dynamic_loading/7
         Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/7");
 
-        //        2. Wait until title is “Dynamic title”
+        //2. Wait until title is “Dynamic title”
 
         //creating the wait object to be able to create certain condition to wait
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
 
         //use the "wait" object to create our expected condition
         wait.until(ExpectedConditions.titleIs("Dynamic title"));
 
-        //create object of DynamicLoad7Page to reach and assert web elements
+        //Create object of DynamicLoad7Page to reach and assert web elements
         DynamicLoad7Page dynamicLoad7Page = new DynamicLoad7Page();
 
-
-        //        3. Assert:Message “Done” is displayed.
+        //3. Assert: Message “Done” is displayed.
         Assert.assertTrue(dynamicLoad7Page.doneMessage.isDisplayed());
 
-        //        4. Assert: Image is displayed.
+        //4. Assert: Image is displayed
         Assert.assertTrue(dynamicLoad7Page.spongeBobImage.isDisplayed());
-
-
 
     }
 
     @Test
     public void dynamic_load_1_test(){
-
-//        TC#5 : Dynamically Loaded Page Elements 1
-//        1. Go to https://practice.cydeo.com/dynamic_loading/1
+        //1. Go to https://practice.cydeo.com/dynamic_loading/1
         Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/1");
-//
-       DynamicLoad1Page dynamicLoad1Page = new DynamicLoad1Page();
-      WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
 
+        DynamicLoad1Page dynamicLoad1Page = new DynamicLoad1Page();
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
 
-//        2. Click to start
+        //2. Click to start
         dynamicLoad1Page.startButton.click();
 
-        //        3. Wait until loading bar disappears
+        //3. Wait until loading bar disappears
         wait.until(ExpectedConditions.invisibilityOf(dynamicLoad1Page.loadingBar));
 
         //4. Assert username inputbox is displayed
@@ -66,11 +58,11 @@ public class WebDriveWaitPractices {
         //6. Enter password: incorrectpassword
         dynamicLoad1Page.inputPassword.sendKeys("somethingwrong");
 
-        //        7. Click to Submit button
+        //7. Click to Submit button
         dynamicLoad1Page.submitButton.click();
 
-//        8. Assert “Your password is invalid!” text is displayed.
+        //8. Assert “Your password is invalid!” text is displayed.
         Assert.assertTrue(dynamicLoad1Page.errorMessage.isDisplayed());
-
     }
+
 }
